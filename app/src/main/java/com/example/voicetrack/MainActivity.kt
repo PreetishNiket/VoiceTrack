d09d68a8707978d98e57e7c98081b2ec658a4953
Preetish Niket <niketpreetish@gmail.com>
2020-09-20 00:23:51 +0530

    Added A custom Toast

--- app/src/main/java/com/example/voicetrack/MainActivity.kt
+++ app/src/main/java/com/example/voicetrack/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.voicetrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+       val layout=layoutInflater.inflate(R.layout.custom_toast,findViewById(R.id.root_container))
+        with(Toast(applicationContext)){
+            setGravity(Gravity.NO_GRAVITY,0,0)
+            duration=Toast.LENGTH_SHORT
+            view=layout
+            show()
+        }
     }
 }
